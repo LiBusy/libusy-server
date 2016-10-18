36e6f3bc10f52d0cab0378e4fd1bdf14a479c9e5
dillonwastrack <libusyapp@gmail.com>
2016-10-18 18:37:59 -0500

    bone

--- app/UserCoordinates.php
+++ app/UserCoordinates.php
@@ -9,4 +9,9 @@ class UserCoordinates extends Model
     protected $table = 'user_marker_coordinates';
     public $timestamps = false;
     protected $fillable = ['lat', 'lng', 'timestamp'];
+
+    protected $casts = [
+        'lat' => 'float',
+        'lng' => 'float'
+    ];
 }
