824fa070e1dc315ccb91610622249d28aaff7fb9
dillonwastrack <libusyapp@gmail.com>
2016-10-25 17:12:29 -0500

    bone

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             //\App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\VerifyApiKey::class
         ],
 
         'api' => [
@@ -53,5 +52,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verify.key' => \App\Http\Middleware\VerifyApiKey::class
     ];
 }
