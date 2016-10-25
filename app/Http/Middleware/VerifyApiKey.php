f6b8c6e5eea6e2f617c0a6cee4f85c35a561438b
dillonwastrack <libusyapp@gmail.com>
2016-10-25 17:09:04 -0500

    bone

--- app/Http/Middleware/VerifyApiKey.php
+++ app/Http/Middleware/VerifyApiKey.php
@@ -18,6 +18,10 @@ class VerifyApiKey
     public function handle($request, Closure $next)
     {
         //333C949CDEEBAB5ED3C747AF3EBBE
+        if ($request->query('key') != '333C949CDEEBAB5ED3C747AF3EBBE')
+        {
+            return redirect('/');
+        }
         //dd($request->query());
         return $next($request);
     }
