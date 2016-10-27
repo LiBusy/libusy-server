30d67527312960b38d4458bfb3bbd47cf642a397
dillonwastrack <libusyapp@gmail.com>
2016-10-27 18:11:54 -0500

    bone

--- app/Http/Controllers/UserMarkerController.php
+++ app/Http/Controllers/UserMarkerController.php
@@ -25,7 +25,7 @@ class UserMarkerController extends Controller
         $marker = new UserCoordinates();
         $marker->lat = $lat;
         $marker->lng = $lng;
-        dd(floatValue($marker->lat));
+        dd((float) $marker->lat);
         if ($marker->lat == "0.0" && $marker->lng = "0.0") // default shared preference value
         {
             return false;
