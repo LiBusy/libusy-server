4bd897e8ed3c264ccb321f3b0d3b2f5537ea0851
dillonwastrack <libusyapp@gmail.com>
2016-11-03 15:31:38 -0500

    bone

--- app/LocationRepository.php
+++ app/LocationRepository.php
@@ -10,7 +10,7 @@ class LocationRepository implements LocationRepositoryInterface
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
         curl_close($ch);
-        return $output;
+        return json_decode($output);
         //$response = http_get($url);
         //return $response;
     }
