f55f9b3d05e33c74c1c34275a034fd92bdacb4f4
dillonwastrack <libusyapp@gmail.com>
2016-11-03 15:23:26 -0500

    bone

--- app/LocationRepository.php
+++ app/LocationRepository.php
@@ -7,7 +7,8 @@ class LocationRepository implements LocationRepositoryInterface
     {
         $url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid='.$placeId.'&key=AIzaSyAseWPTr6P88XO1gm78yUq5MkR9pu1t7jA';
         $ch = curl_init($url);
-        return $ch;
+        $output = curl_exec($ch);
+        return $output;
         //$response = http_get($url);
         //return $response;
     }
