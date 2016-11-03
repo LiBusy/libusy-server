193453b0dc6b9a1457f54e75f8d29470723b1d2f
dillonwastrack <libusyapp@gmail.com>
2016-11-02 22:43:14 -0500

    bone

--- app/Http/Controllers/MarkerController.php
+++ app/Http/Controllers/MarkerController.php
@@ -18,7 +18,7 @@ class MarkerController extends Controller
     protected $locations;
 
 
-    public function __construct(LibraryBusyness $locations) //\App\Contracts\LocationRepositoryInterface $locations
+    public function __construct(LocationRepositoryInterface $locations) //\App\Contracts\LocationRepositoryInterface $locations
     {
         dd(LocationRepositoryInterface::class);
         $this->locations= $locations;
