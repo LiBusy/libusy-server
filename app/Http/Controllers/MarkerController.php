1251e3f1eda958fcdef6e9e22f928bc0234a44a2
dillonwastrack <libusyapp@gmail.com>
2016-11-03 15:38:06 -0500

    bone

--- app/Http/Controllers/MarkerController.php
+++ app/Http/Controllers/MarkerController.php
@@ -32,7 +32,7 @@ class MarkerController extends Controller
         foreach ($markers as $marker)
         {
             $place = $this->locations->getAll($marker->place_id);
-            dd($place->result['formatted_phone_number']);
+            dd($place->result->formatted_phone_number);
 
 
         }
