12f277c5742f00e5caa6a0e4c1d1e83b15c6a63f
dillonwastrack <libusyapp@gmail.com>
2016-11-15 13:02:12 -0600

    changing to library api

--- app/Support/LocationRepository.php
+++ app/Support/LocationRepository.php
@@ -14,6 +14,7 @@ class LocationRepository implements LocationRepositoryInterface
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
+        dd($output);
         curl_close($ch);
         return json_decode($output);
         //$response = http_get($url);
