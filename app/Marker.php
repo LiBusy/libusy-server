554ce5627e03a469c4b49785b071de3f65d39f83
dillonwastrack <libusyapp@gmail.com>
2016-11-24 15:32:33 -0600

    test

--- app/Marker.php
+++ app/Marker.php
@@ -42,7 +42,7 @@ class Marker extends Model
     {
         return $this->attributes['check_ins'] = UserCoordinates::where('library', '=', $this->attributes['library'])
                                                                 ->where('timestamp', '>=', Carbon::parse('1 hours ago'))
-                                                                ->count().' check-ins in the past hour.';
+                                                                ->count().' bones in the past hour.';
     }
 
 
